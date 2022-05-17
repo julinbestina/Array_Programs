aca722e613ec5b6827e0386b2cceecfe86c6313a
julinbestina <julin.bestina@yahoo.com>
2022-05-17 15:19:54 +0530

    Added code to print String array

---
+++ NameArray.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+	public class  NameArray {
+		public static void main(String[] args) {
+			String[] name = new String[5];
+			Scanner scanner = new Scanner(System.in);
+			System.out.print("Enter 5 names:");
+			for ( int i = 0; i < 5; i++) {
+				name[i] = scanner.nextLine();
+			}
+			for( int i = 0; i < 5; i++) {
+				System.out.println("Hi " + name[i]);
+			}
+		}
+	}
