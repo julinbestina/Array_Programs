cbe185346667ee691e1ec4d554b71c18870c0502
julinbestina <julin.bestina@yahoo.com>
2022-05-18 08:29:56 +0530

    Refactored loop condition

--- ArrayOfNum.java
+++ ArrayOfNum.java
@@ -1,10 +1,10 @@
 	public class ArrayOfNum {
 		public static void main(String[] args) {
 			int[] number = new int[10];
-			for( int i = 0; i < 10; i++) {
+			for( int i = 0; i < number.length; i++) {
 				number[i] = i+1;
 			}
-			for( int i = 0; i < 10; i++) {
+			for( int i = 0; i < number.length; i++) {
 				System.out.println("index" +" " + i +" = " + number[i]);
 			}
 		}
