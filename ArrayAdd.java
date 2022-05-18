9c6c349dd7db0cecf210c270a3dcba422b8f8b10
julinbestina <julin.bestina@yahoo.com>
2022-05-18 08:23:21 +0530

    Refactored loop condition

--- ArrayAdd.java
+++ ArrayAdd.java
@@ -2,11 +2,11 @@
 	public class ArrayAdd {
 		public static void main(String[] args) {
 			int[] num = new int[20];
-			for( int i = 0; i < 20; i++) {
+			for( int i = 0; i < num.length; i++) {
 				num[i] = i+1;
 			}
 			int add = 0;
-			for( int i = 0; i < 20; i++) {
+			for( int i = 0; i < num.length; i++) {
 				add += i;
 			}
 			System.out.println("Addition of given numbers = "+ add);
